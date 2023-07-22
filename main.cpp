0b6573de4c57920962714575fa3debaf02541803
ENKohanko <grindar10101@gmail.com>
2023-07-23 00:48:07 +0300

    Added check file extension

--- main.cpp
+++ main.cpp
@@ -12,9 +12,16 @@ int main(int argc, char *argv[]) {
   QString path = QString(argv[1]);
   QFileInfo file(path);
   if (!file.exists()) {
-    std::cout << "Такого файла не существует" << std::endl;
+    std::cout << "Ошибка: такого файла не существует" << std::endl;
     exit(0);
   }
+  QString ext = file.completeSuffix();
+  if (!QStringList({"sqlite", "sqlite3", "db", "db3", "s3db", "sl3"})
+           .contains(ext)) {
+    std::cout << "Ошибка: файл имеет не правильное расширение!" << std::endl;
+    exit(0);
+  }
+
   QString absultePath = file.absoluteFilePath();
 
   QApplication a(argc, argv);
