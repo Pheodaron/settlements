af5af8c3ff45b9a612dd2859142260465268b167
ENKohanko <grindar10101@gmail.com>
2023-07-23 19:40:32 +0300

    Added reading default file path

--- main.cpp
+++ main.cpp
@@ -4,28 +4,35 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+  QString absolutePath;
   if (argc == 1) {
-    std::cout << "Ошибка: не задан путь к бд" << std::endl;
-    exit(0);
+    QFileInfo file("./cities.db3");
+    if (file.exists()) {
+      absolutePath = file.absoluteFilePath();
+    } else {
+      std::cout << "Ошибка: в текущей директории не найден файл базы по "
+                   "умолчанию или не задан путь к бд"
+                << std::endl;
+      exit(0);
+    }
+  } else {
+    QString path = QString(argv[1]);
+    QFileInfo file(path);
+    if (!file.exists()) {
+      std::cout << "Ошибка: такого файла не существует" << std::endl;
+      exit(0);
+    }
+    QString ext = file.completeSuffix();
+    if (!QStringList({"sqlite", "sqlite3", "db", "db3", "s3db", "sl3"})
+             .contains(ext)) {
+      std::cout << "Ошибка: файл имеет не правильное расширение!" << std::endl;
+      exit(0);
+    }
+    absolutePath = file.absoluteFilePath();
   }
-
-  QString path = QString(argv[1]);
-  QFileInfo file(path);
-  if (!file.exists()) {
-    std::cout << "Ошибка: такого файла не существует" << std::endl;
-    exit(0);
-  }
-  QString ext = file.completeSuffix();
-  if (!QStringList({"sqlite", "sqlite3", "db", "db3", "s3db", "sl3"})
-           .contains(ext)) {
-    std::cout << "Ошибка: файл имеет не правильное расширение!" << std::endl;
-    exit(0);
-  }
-
-  QString absultePath = file.absoluteFilePath();
 
   QApplication a(argc, argv);
-  MainWindow w(absultePath);
+  MainWindow w(absolutePath);
   w.show();
   return a.exec();
 }
