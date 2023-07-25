39c7a3e4b183ad6a4d0169d8c44758c8330c3f5a
ENKohanko <grindar10101@gmail.com>
2023-07-25 23:35:38 +0300

    Changed default db file from /etc/ to /var/

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   QString absolutePath;
   if (argc == 1) {
 #ifdef __linux__
-    QFileInfo file("/etc/cities.db3");
+    QFileInfo file("/var/cities.db3");
 #elif _WIN32
     QFileInfo file("./cities.db3");
 #endif
